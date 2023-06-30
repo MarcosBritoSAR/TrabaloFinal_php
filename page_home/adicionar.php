a709ea5dcaebc055aebb3f055e65e10607413513
kennedyBJJ <williankennedy368@gmail.com>
2023-06-30 12:25:05 -0300

    Adicionado alterações no BD e cadastro de eventos

--- page_home/adicionar.php
+++ page_home/adicionar.php
@@ -1,3 +1,12 @@
+<!--Vai receber os valores do banco de dados-->
+<?php 
+require_once("../conexao.php");
+$result = array();
+if (isset($_COOKIE['user'])) {
+    //Acessando o banco de dados
+    $result = busca($_COOKIE['user']);
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 
@@ -5,7 +14,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
+    <script src="./src/script.js"></script>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
@@ -14,53 +23,97 @@
 
     <nav class="menu-lateral">
 
-            <a href="../destroiLogin.php">
-                <div class="deslogar">
-                    <i class="bi bi-box-arrow-left"></i>
-                </div>
-            </a>
+        <a href="../destroiLogin.php">
+            <div class="deslogar">
+                <i class="bi bi-box-arrow-left"></i>
+            </div>
+        </a>
 
-            <ul>
-                <li class="item-menu">
-                    <a href="home.php">
-                        <span class="icon"><i class="bi bi-house"></i></span>
-                        <span class="txt-link">Home</span>
-                    </a>
-                </li>
+        <ul>
+            <li class="item-menu">
+                <a href="home.php">
+                    <span class="icon"><i class="bi bi-house"></i></span>
+                    <span class="txt-link">Home</span>
+                </a>
+            </li>
 
-                <li class="item-menu">
-                    <a href="adicionar.php">
-                        <span class="icon"><i class="bi bi-plus-square-fill"></i></span>
-                        <span class="txt-link">Adicionar</span>
-                    </a>
-                </li>
+            <li class="item-menu">
+                <a href="adicionar.php">
+                    <span class="icon"><i class="bi bi-plus-square-fill"></i></span>
+                    <span class="txt-link">Adicionar</span>
+                </a>
+            </li>
 
-                <li class="item-menu">
-                    <a href="perfil.php">
-                        <span class="icon"><i class="bi bi-person-fill"></i></span>
-                        <span class="txt-link">perfil</span>
-                    </a>
-                </li>
+            <li class="item-menu">
+                <a href="perfil.php">
+                    <span class="icon"><i class="bi bi-person-fill"></i></span>
+                    <span class="txt-link">perfil</span>
+                </a>
+            </li>
 
-                <li class="item-menu">
-                    <a href="proximos.php">
-                        <span class="icon"><i class="bi bi-fast-forward"></i></span>
-                        <span class="txt-link">Proximos</span>
-                    </a>
-                </li>
+            <li class="item-menu">
+                <a href="proximos.php">
+                    <span class="icon"><i class="bi bi-fast-forward"></i></span>
+                    <span class="txt-link">Proximos</span>
+                </a>
+            </li>
 
-                <li class="item-menu">
-                    <a href="passados.php">
-                        <span class="icon"><i class="bi bi-bell-fill"></i></span>
-                        <span class="txt-link">Passados</span>
-                    </a>
-                </li>
+            <li class="item-menu">
+                <a href="passados.php">
+                    <span class="icon"><i class="bi bi-bell-fill"></i></span>
+                    <span class="txt-link">Passados</span>
+                </a>
+            </li>
 
-            </ul>
+        </ul>
 
-        </nav>
+    </nav>
 
-        <h1 style="text-align: center">adicionar</h1>
+    <h1 style="text-align: center">adicionar</h1>
+
+    <div class="cadastro">
+        <form class="formulario-cadastro" action="cadastrar_evento.php" method="post">
+            <input type="hidden" name="id_usuario" value="<?php echo $result[0]['id']; ?>">
+
+            <span>
+                <label for="nome">Ocasião</label>
+                <input type="text" name="name">
+            </span>
+
+            <span>
+                <label for="data-inicio">Data de inicio</label>
+                <input type="date" name="data-inicio">
+            </span>
+
+            <span>
+
+                <p>Informe em quanto tempo deseja ser lembrado</p>
+                <label for="dias">Dias: </label>
+                <input type="number" name="dias">
+                <br>
+                <label for="meses">Meses: </label>
+                <input type="number" name="meses">
+                <br>
+
+                <label for="anos">Anos: </label>
+                <input type="number" name="anos">
+            </span>
+
+            <span class="container-mensagem">
+                <label for="mensagem">Deixe uma mensagem!(Máximo de 500 caracteres)</label>
+                <br>
+                <textarea cols="50" rows="10" name="mensagem" id="mensagem"></textarea>
+            </span>
+            <span class="bts-cadastro">
+                <button class="bt-cadastro" type="submit">Cadastrar</button>
+                <button class="bt-limpar" type="reset">Limpar</button>
+            </span>
+
+        </form>
+
+        <button class="acoes" onclick="gerarMensagem()">Gerar mensagem!</button>
+    </div>
+
 </body>
 
 </html>
--- page_home/cadastrar_evento.php
+++ page_home/cadastrar_evento.php
@@ -9,25 +9,20 @@ $anos = intval($_POST["anos"]);
 $mensagem = $_POST["mensagem"];
 
 $diasTotais = $dias + $meses * 30 + $anos * 12 * 30;
-require_once("conexao.php");
+require_once("../conexao.php");
 
 //converter a string dataInicio em data usando UnixTimeStamp e somando os dias que devem ser lembrados
 $UnixStampDataLembrete = strtotime($dataInicio . "+$diasTotais days");
 
 $dataLembrete = date('Y/m/d', $UnixStampDataLembrete);
 
-$query = "INSERT INTO evento (data_registro, data_lembrete, nome, id_usuario)
-        VALUES ('$dataInicio', '$dataLembrete', '$name', '$id_usuario')";
+$query = "INSERT INTO evento (data_registro, data_lembrete, nome, id_usuario, mensagem)
+        VALUES ('$dataInicio', '$dataLembrete', '$name', '$id_usuario', '$mensagem')";
 
 if (mysqli_query($conexao, $query)) {
-    session_start();
-    $_SESSION['sucessoCadastro'] = true;
-    header("Location: index.php");
+    header("Location: home.php");
 } else {
-    session_start();
-    $_SESSION['sucessoCadastro'] = false;
-    header("Location: index.php");
-
+    echo "Houve erro no Cadastro :(!";
 }
 
 ?>
